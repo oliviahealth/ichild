fbee84c4a2a09b2cd5315b3a16babec6ee29692e
SumitNalavade <vs.nalavade2003@gmail.com>
2023-06-18 18:13:34 -0500

    Setup new project with React, Vite and Tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        tungsten: ['Tungsten', 'sans-serif']
+      }
+    },
+  },
+  plugins: [require("daisyui")],
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          "primary": "#661429",
+          "secondary": "#003c71",
+        }
+      }
+    ]
+  }
+}
