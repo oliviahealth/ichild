eeceba5fdf8ac71f39cc47e4af3af86ded067eac
SumitNalavade <vs.nalavade2003@gmail.com>
2023-06-18 19:02:02 -0500

    Add Navbar

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,18 +1,21 @@
 import React from "react";
 
+import Navbar from "./Navbar";
+
 interface Props {
-    children: React.ReactNode
+  children: React.ReactNode;
 }
 
 const Layout: React.FC<Props> = ({ children }) => {
-    return (
-        <div className="flex flex-col h-screen">
+  return (
+    <div className="flex flex-col h-screen text-black">
+      <div className="shadow-md">
+        <Navbar />
+      </div>
 
-            <div className="container mx-auto px-4 flex-1">
-                { children }
-            </div>
-        </div>
-    )
-}
+      <div className="container mx-auto px-4 flex-1">{children}</div>
+    </div>
+  );
+};
 
 export default Layout;
--- tailwind.config.js
+++ tailwind.config.js
@@ -18,6 +18,7 @@ export default {
         mytheme: {
           "primary": "#661429",
           "secondary": "#003c71",
+          "neutral": "#3D4451",
         }
       }
     ]
