70c307c46125a7f0d5a6d77f3ca284ea794d5f3a
Sumit Nalavade <vs.nalavade2003@gmail.com>
2023-08-14 00:47:08 -0500

    Fix scrollbar on Saved Locations page

--- src/components/Chat/OllieAvatar.tsx
+++ src/components/Chat/OllieAvatar.tsx
@@ -5,8 +5,8 @@ import Ollie from "../../assets/ollie.png";
 const OllieAvatar: React.FC = () => {
     return (
         <div className={`chat-image avatar `}>
-            <div className="w-10 rounded-full">
-                <img src={Ollie} />
+            <div className="w-12 rounded-full">
+                <img className="shadow-2xl" src={Ollie} />
             </div>
         </div>
     )
