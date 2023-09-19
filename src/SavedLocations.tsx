837c5b3b90d5cb61ddadacd5009bb51dcb82ed43
Sumit Nalavade <vs.nalavade2003@gmail.com>
2023-09-19 12:29:15 -0500

    UI Updates

--- src/SavedLocations.tsx
+++ src/SavedLocations.tsx
@@ -4,20 +4,21 @@ import { useMutation } from "react-query";
 import useAppStore from "./stores/useAppStore";
 import fetchWithAxios from "./utils/fetchWithAxios";
 import parseWithZod from "./utils/parseWithZod";
-import { SavedLocationSchema, ISavedLocation } from "./utils/interfaces";
+import { SavedLocationSchema, ISavedLocation, ILocation } from "./utils/interfaces";
 
 import ChatBubble from "./components/Chat/ChatBubble";
 import OllieAvatar from "./components/Chat/OllieAvatar";
-import InteractiveMap from "./components/Chat/InteractiveMap";
 import LoadingSkeleton from "./components/LoadingSkeleton";
 import { TfiMenuAlt } from "react-icons/tfi";
 import { BiCopy } from "react-icons/bi";
 import { BiSolidBookmark } from "react-icons/bi";
+import LocationInfoPanel from "./components/Chat/LocationInfoPanel";
 
 const SavedLocations: React.FC = () => {
     const user = useAppStore((state) => state.user);
 
     const [savedLocations, setSavedLocations] = useState<null | ISavedLocation[]>(null);
+    const [focusedLocation, setFocusedLocation] = useState<null | ILocation>(null);
     const [deletingLocationName, setDeletingLocationName] = useState<null | string>(null);
 
     const isSidePanelOpen = useAppStore((state) => state.isSidePanelOpen);
@@ -58,28 +59,6 @@ const SavedLocations: React.FC = () => {
         }
     }, [user])
 
-    // Convert milliseconds to a formatted date and time
-    function convertMillisecondsToFormattedDateTime(milliseconds: number) {
-        const date = new Date(milliseconds);
-        const year = date.getFullYear();
-        const month = date.getMonth(); // Month is 0-based
-        const day = date.getDate();
-        const hours = date.getUTCHours();
-        const minutes = date.getUTCMinutes();
-        const period = hours < 12 ? 'AM' : 'PM';
-        const formattedHours = (hours % 12 === 0 ? 12 : hours % 12).toString().padStart(2, '0');
-        const formattedMinutes = minutes.toString().padStart(2, '0');
-
-        const formattedDate = `${year}-${(month + 1).toString().padStart(2, '0')}-${day.toString().padStart(2, '0')}`;
-        const formattedTime = `${formattedHours}:${formattedMinutes} ${period}`;
-
-        return {
-            formattedDate,
-            formattedTime
-        };
-    }
-
-
     return (
         <div className="flex w-full flex-col h-full p-4">
             {!isSidePanelOpen && (
@@ -92,58 +71,52 @@ const SavedLocations: React.FC = () => {
                 <LoadingSkeleton />
             )}
 
-            <div className="flex flex-col gap-6 overflow-y-auto max-h-[calc(100vh-11rem)]">
-                {savedLocations?.map((location, index) => {
-                    const { formattedDate, formattedTime } = convertMillisecondsToFormattedDateTime(location.dateCreated);
+            <div className="flex overflow-y-auto max-h-[calc(100vh-11rem)]">
+                <div className="w-1/2">
+                    {savedLocations?.map((location, index) => (
+                        <div key={index} className="xl:flex gap-4 my-4">
+                            <div>
+                                <OllieAvatar />
+                            </div>
 
-                    return (
-                    <div key={index} className="xl:flex xl:items-center gap-4">
-                        <div>
-                            <OllieAvatar />
-                        </div>
+                            <div onClick={() => setFocusedLocation(location)}>
+                                <ChatBubble isResponse={true} >
+                                    <div className="flex justify-between items-center p-1 sm:w-[27rem]">
+                                        <div className="flex items-center gap-6 w-full">
+                                            {/* Render the letters of the alphabet starting with 'A' */}
+                                            <p className="text-3xl text-primary">{String.fromCharCode(65 + index)}</p>
 
-                        <div>
-                            <ChatBubble isResponse={true} >
-                                <div className="flex justify-between items-center p-1 sm:w-[27rem]">
-                                    <div className="flex items-center gap-6 w-full">
-                                        {/* Render the letters of the alphabet starting with 'A' */}
-                                        <p className="text-3xl text-primary">{String.fromCharCode(65 + index)}</p>
+                                            <div className="w-full">
+                                                <p className="font-semibold">{location.name}</p>
 
-                                        <div className="w-full">
-                                            <p className="font-semibold">{location.name}</p>
+                                                <div className="flex gap-2 my-3">
+                                                    <a className="btn btn-xs border-none bg-gray-200 text-black hover:bg-gray-300">Website</a>
+                                                    <a href={location.addressLink} target="_blank" className="btn btn-xs border-none bg-gray-200 text-black hover:bg-gray-300">Directions</a>
+                                                </div>
 
-                                            <div className="flex gap-2 my-3">
-                                                <a className="btn btn-xs border-none bg-gray-200 text-black hover:bg-gray-300">Website</a>
-                                                <a href={location.addressLink} target="_blank" className="btn btn-xs border-none bg-gray-200 text-black hover:bg-gray-300">Directions</a>
+                                                <p className="text-sm">{location.address}</p>
                                             </div>
+                                        </div>
 
-                                            <p className="text-sm">{location.address}</p>
+                                        <div className="flex flex-col gap-6">
+                                            {user && (<button onClick={() => deleteSavedLocation(location.name)} className={`btn btn-square btn-xs bg-inherit border-none ml-4 hover:bg-gray-200`} >
+                                                {isDeleteLoading && deletingLocationName === location.name ? (<span className="loading loading-spinner loading-sm"></span>) : (<BiSolidBookmark className="text-xl text-black" />)}
+                                            </button>)}
+
+                                            <button className={`btn btn-square btn-xs bg-inherit border-none ml-4 hover:bg-gray-200`} onClick={() => copyText(location.address)}>
+                                                <BiCopy className="text-xl text-black" />
+                                            </button>
                                         </div>
                                     </div>
-
-                                    <div className="flex flex-col gap-6">
-                                        {user && (<button onClick={() => deleteSavedLocation(location.name)} className={`btn btn-square btn-xs bg-inherit border-none ml-4 hover:bg-gray-200`} >
-                                            {isDeleteLoading && deletingLocationName === location.name ? (<span className="loading loading-spinner loading-sm"></span>) : (<BiSolidBookmark className="text-xl text-black" />)}
-                                        </button>)}
-
-                                        <button className={`btn btn-square btn-xs bg-inherit border-none ml-4 hover:bg-gray-200`} onClick={() => copyText(location.address)}>
-                                            <BiCopy className="text-xl text-black" />
-                                        </button>
-                                    </div>
-                                </div>
-                            </ChatBubble>
+                                </ChatBubble>
+                            </div>
                         </div>
+                    ))}
+                </div>
 
-                        <div className="max-w-[29rem] w-full h-32 p-3 bg-white rounded-xl flex justify-center items-center">
-                            { location.latitude && location.longitude ? (<InteractiveMap center={{ lat: location.latitude, lng: location.longitude }} locations={[location]} />) : <InteractiveMap /> }
-                        </div>
-
-                        <div className="text-sm hidden xl:block">
-                            <p>{formattedTime}</p>
-                            <p>{formattedDate}</p>
-                        </div>
-                    </div>)
-                })}
+                <div className="w-1/2">
+                    {  focusedLocation && (<LocationInfoPanel location={focusedLocation} />) }
+                </div>
             </div>
         </div>
     )
---
+++ src/components/Chat/LocationInfoPanel.tsx
@@ -0,0 +1,78 @@
+import React, { useState } from "react";
+import { ILocation } from "../../utils/interfaces";
+
+import { MdOutlineOpenInNew } from "react-icons/md";
+import PanoramicStreetView from "./PanoramicStreetView";
+import InteractiveMap from "./InteractiveMap";
+
+interface Props {
+    location: ILocation
+}
+
+const LocationInfoPanel: React.FC<Props> = ({ location }) => {
+    const currentDayOfWeek = new Date().getDay()
+    const [descriptionExpanded, setDescriptionExpanded] = useState(false);
+
+    return (
+        <div className="my-4 flex flex-col gap-4 bg-[#F8F5F5] p-3 rounded-xl">
+            <div className="h-80">
+                {location.streetViewExists && location.latitude && location.longitude ? <PanoramicStreetView latitude={location.latitude} longitude={location.longitude} /> : <InteractiveMap locations={[location]} />}
+            </div>
+
+            <p className="font-semibold text-2xl">{location.name}</p>
+
+            <div className="flex gap-x-1 items-center">
+                <div className="rating rating-md rating-half flex items-center gap-2">
+                    {location.rating && (<>
+                        <div>
+                            {Array.from({ length: Math.round(location.rating * 2) }).map((_, elm) => (
+                                <span key={`Rating: ${elm}`}>{(elm * 0.5) === Math.floor(elm * 0.5) ? <input type="radio" name="rating-10" className="bg-yellow-500 mask mask-star-2 mask-half-1" /> : <input type="radio" name="rating-10" className="bg-yellow-500 mask mask-star-2 mask-half-2" />}</span>
+                            ))}
+                        </div>
+                        <span>({location.rating})</span></>)}
+                </div>
+            </div>
+
+            <div className="collapse collapse-arrow bg-opacity-50 bg-gray-200 text-sm">
+                <input type="checkbox" className="peer" />
+                <div className="collapse-title flex justify-between items-center font-semibold">
+                    <p>{["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"][currentDayOfWeek]}</p>
+                    <p>{Object.values(location.hoursOfOperation[currentDayOfWeek])[0].split(": ")[1]}</p>
+                </div>
+                <div className="collapse-content">
+                    {location.hoursOfOperation.map((hours, index) => {
+                        const dayOfWeek = Object.keys(hours)[0];
+                        const capitalizedDayOfWeek = dayOfWeek.charAt(0).toUpperCase() + dayOfWeek.slice(1);
+
+                        return (
+                            <div key={index} className={`flex justify-between my-1 ${currentDayOfWeek === index ? 'font-semibold' : ''}`}>
+                                <p key={index}>{capitalizedDayOfWeek}</p>
+
+                                <p key={index}>{Object.values(hours)[0].split(": ")[1]}</p>
+                            </div>
+                        )
+                    })}
+                </div>
+            </div>
+
+            <p className={`text-sm ${!descriptionExpanded ? "line-clamp-4" : ""}`} onClick={() => setDescriptionExpanded(!descriptionExpanded)} >{location.description}</p>
+
+            <div className="flex gap-x-1 items-center">
+                <p className="font-semibold">Website: </p>
+                <a className="text-primary" href={location.website} target="_blank">{location.website}</a>
+            </div>
+
+            <div className="flex gap-x-1 items-center">
+                <p className="font-semibold">Phone: </p>
+                <a href={`tel:${location.phone}`} target="_blank">{location.phone}</a>
+            </div>
+
+            <a href={location.addressLink} target="_blank" className={`max-w-[200px] btn btn-xs text-black bg-gray-300 border-none hover:bg-gray-400`}>
+                <MdOutlineOpenInNew className="text-lg" />
+                <p>Open Google Maps</p>
+            </a>
+        </div>
+    )
+}
+
+export default LocationInfoPanel;
